1acb05539a9dda5a0ee6433f5fa7d963e4a01237
waltur <tur-87@hotmail.com>
2025-05-18 20:19:48 -0500

    conexion base produccion

--- db.js
+++ db.js
@@ -1,12 +1,20 @@
 const { Pool } = require('pg');
 require('dotenv').config();
-
+/*
 const pool = new Pool({
   user: process.env.DB_USER,
   host: process.env.DB_HOST,
   database: process.env.DB_NAME,
   password: process.env.DB_PASSWORD,
   port: process.env.DB_PORT,
+});
+*/
+const pool = new Pool({
+  user: "worksheet_ter9_user",
+  host: "dpg-d0kv41d6ubrc73bl1qdg-a",
+  database: "worksheet_ter9",
+  password: "7QA2KEif36I5m2Va5agw2Ch2UBvxfMWE",
+  port: "5432",
     ssl: {
       rejectUnauthorized: false
     }

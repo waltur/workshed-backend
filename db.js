d9d65ac67532f62b8be762e15b8cebea877c2f58
waltur <tur-87@hotmail.com>
2025-05-18 20:30:11 -0500

    conexion base produccion

--- db.js
+++ db.js
@@ -10,11 +10,13 @@ const pool = new Pool({
 });
 */
 const pool = new Pool({
-  user: "worksheet_ter9_user",
-  host: "dpg-d0kv41d6ubrc73bl1qdg-a",
-  database: "worksheet_ter9",
-  password: "7QA2KEif36I5m2Va5agw2Ch2UBvxfMWE",
-  port: "5432",
+ const pool = new Pool({
+   user: process.env.DB_USER,
+   host: process.env.DB_HOST,
+   database: process.env.DB_NAME,
+   password: process.env.DB_PASSWORD,
+   port: process.env.DB_PORT,
+
     ssl: {
       rejectUnauthorized: false
     }

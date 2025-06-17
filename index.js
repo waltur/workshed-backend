ca7761ea25bb5132d58ac378598b1ffb74c6a0a4
waltur <tur-87@hotmail.com>
2025-06-16 19:09:13 -0500

    tamaño de imagen

--- index.js
+++ index.js
@@ -7,7 +7,9 @@ const port = process.env.PORT || 3000;
 
 // Middlewares
 app.use(cors());
-app.use(express.json());
+//app.use(express.json());
+app.use(express.json({ limit: '1mb' }));
+app.use(express.urlencoded({ extended: true, limit: '1mb' }));
 
 // Rutas
 const indexRoutes = require('./routes/index');
